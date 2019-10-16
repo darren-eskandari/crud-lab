043aade4a40e30f6a96e492bb71fd25eb05d8b09
GA SEI <GA-SEI@Darrens-MacBook-Pro-2.local>
2019-10-16 15:35:01 -0700

    add edit edit route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const Monsters = require('./Models/monsters.js');
 const methodOverride = require('method-override');
-
+const bodyParser = require('body-parser');
 
 // set port
 const PORT = 3000;
@@ -10,9 +10,31 @@ const PORT = 3000;
 
 // middleware
 app.use(methodOverride('_method'));
-
+app.use(bodyParser.urlencoded({extended: false}));
 
 // routes
+
+// edit rout - get
+app.get('/monsters/:index/edit', (req, res) => {
+    res.render('edit.ejs', {
+        monsters: Monsters[req.params.index],
+        index: req.params.index,
+    });
+});
+
+// edit route - put
+app.put('/monsters/:index', (req, res) => {
+    console.log(req.body, '<--- THIS IS REQ.BODY')
+    const updatedMonster = {
+        name: Monsters[req.params.index].name,
+        ac: req.body.ac,
+        hp: req.body.hp,
+        att: req.body.att,
+        dmg: req.body.dmg
+    }
+    Monsters.splice(req.params.index, 1, updatedMonster);
+    res.redirect('/monsters');
+});
 
 
 // index route
