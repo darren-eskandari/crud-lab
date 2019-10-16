1b8a5cdc4504012536224cf013aeda4586d271a9
GA SEI <GA-SEI@Darrens-MacBook-Pro-2.local>
2019-10-16 14:39:27 -0700

    add show.ejs and delete routes

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const app = express();
-const Monster = require('./Models/monsters.js');
+const Monsters = require('./Models/monsters.js');
+const methodOverride = require('method-override');
+
 
 // set port
 const PORT = 3000;
 
+
+// middleware
+app.use(methodOverride('_method'));
+
+
+// routes
+
+
+// index route
 app.get('/monsters', (req, res) => {
     res.render('index.ejs', {
-        monster: Monster
+        monsters: Monsters
     })
 });
 
+// show route
+app.get('/monsters/:index', (req, res) => {
+    res.render('show.ejs', { 
+        monsters: Monsters[req.params.index] 
+    });
+});
+
+// delete route
+app.delete('/monsters/:index', (req, res) => {
+    console.log(req.params.index);
+    Monsters.splice(req.params.index, 1);
+    res.redirect('/monsters');
+});
 
 
 
